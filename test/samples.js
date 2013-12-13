46b6f5435c1c6e531b78525ea583116f1b1181f5
C. Scott Ananian <cscott@cscott.net>
2013-12-13 15:44:51 -0500

    Basic skeleton, ripped from mw-ocg-latexer and stripped down.

---
+++ test/samples.js
@@ -0,0 +1,32 @@
+/* global describe, it */
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+
+var texter = require('../');
+
+// ensure that we don't crash on any of our sample inputs
+describe("Basic crash test", function() {
+	['tao.zip'].forEach(function(bundle) {
+		describe(bundle, function() {
+			it('should compile to plaintext', function(done) {
+				this.timeout(0);
+				var filename = path.join(__dirname, '..', 'samples', bundle);
+				return texter.convert({
+					bundle: filename,
+					output: filename + '.txt',
+					log: function() { /* suppress logging */ }
+				}).then(function(statusCode) {
+					assert.equal(statusCode, 0);
+				}).ensure(function() {
+					try {
+						fs.unlinkSync(filename + '.' + dest);
+					} catch (e) { }
+				}).done(
+					function() { done(); },
+					function(err) { done(err); }
+				);
+			});
+		});
+	});
+});
