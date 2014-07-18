851b715cc0f065af9557597c658de22ce82e0fca
C. Scott Ananian <cscott@cscott.net>
2014-07-18 00:43:01 -0400

    Use promise support in mocha.
    Change-Id: I474de21cf33acb165d3dc1e7fbcd290466e43fca

--- test/samples.js
+++ test/samples.js
@@ -13,7 +13,7 @@ var texter = require('../');
 describe("Basic crash test", function() {
 	['tao.zip', 'us.zip', 'std_dev.zip'].forEach(function(bundle) {
 		describe(bundle, function() {
-			it('should compile to plaintext', function(done) {
+			it('should compile to plaintext', function() {
 				this.timeout(0);
 				var filename = path.join(__dirname, '..', 'samples', bundle);
 				return texter.convert({
@@ -26,10 +26,7 @@ describe("Basic crash test", function() {
 					try {
 						fs.unlinkSync(filename + '.txt');
 					} catch (e) { }
-				}).done(
-					function() { done(); },
-					function(err) { done(err); }
-				);
+				});
 			});
 		});
 	});
