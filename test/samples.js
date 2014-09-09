c0360962459362e2de8cd2bbc44e7516a6037f9d
C. Scott Ananian <cscott@cscott.net>
2014-09-09 11:05:43 -0400

    Use new log and status reporting framework
    Log through the service! Remove dependency on syslog. Expose all
    errors through the service by always rejecting the main application
    promise if anything goes wrong.
    
    Relies on: https://gerrit.wikimedia.org/r/#/c/151224
    
    Change-Id: I330ebeba8c91d50ee7fd22998c3b94fc027e7c8f

--- test/samples.js
+++ test/samples.js
@@ -20,8 +20,9 @@ describe("Basic crash test", function() {
 					bundle: filename,
 					output: filename + '.txt',
 					log: function() { /* suppress logging */ }
-				}).then(function(statusCode) {
-					assert.equal(statusCode, 0);
+				}).then(function(_) {
+					// should resolve with no value
+					assert.equal(_, undefined);
 				}).finally(function() {
 					try {
 						fs.unlinkSync(filename + '.txt');
